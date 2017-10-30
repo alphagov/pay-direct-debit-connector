f3830c54a32d427df77341ccfb9bd108e3c137e1
Raul Garcia <raul.garcia@digital.cabinet-office.gov.uk>
2017-10-30 09:28:35 +0000

    PP-2808 Initial microservice structure
        - Dropwizard application with Healthcheck.
    
        - Created but commented out for now database related modules (Guice)
        and JPA dependencies. This is awaiting AWS environment to be ready and
        will be enabled in a future PR.
    
        - Environment variables for starting up the microservice for now are:
    
          PORT=10200
          ADMIN_PORT=10201
          DB_HOST=directdebit.db.pymnt.localdomain
          DB_SSL_OPTION=ssl\\\=true
          DB_USER=directdebit
          DB_PASSWORD=<dbpassword>
          JPA_LOG_LEVEL=WARNING
          JPA_SQL_LOG_LEVEL=WARNING
          AUTH_READ_TIMEOUT_SECONDS=2
          CERTS_PATH=/Users/<username>/WORKSPACE/pay-scripts/services/ssl/certs

---
+++ src/main/java/uk/gov/pay/directdebit/app/config/DirectDebitConfig.java
@@ -0,0 +1,23 @@
+package uk.gov.pay.directdebit.app.config;
+
+import io.dropwizard.Configuration;
+
+//TODO: Awaiting AWS DB environment ready
+public class DirectDebitConfig extends Configuration{
+
+//    @Valid
+//    @NotNull
+//    private DataSourceFactory dataSourceFactory;
+//
+//    @Valid
+//    @NotNull
+//    private JPAConfiguration jpaConfiguration;
+//
+//    public DataSourceFactory getDataSourceFactory() {
+//        return dataSourceFactory;
+//    }
+//
+//    public JPAConfiguration getJpaConfiguration() {
+//        return jpaConfiguration;
+//    }
+}
