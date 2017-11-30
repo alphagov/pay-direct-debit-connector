81ad059eb0436483d9c8c5fe966cc7f63850b79f
Raul Garcia <raul.garcia@thenesor.com>
2017-11-30 17:21:10 +0000

    PP-3024 Refactor Integration tests approach
        - Add Test Suite to run all the integration tests.
    
        - Start the application, migrate and create database template before
        the tests start.
    
        - Create Rule that restore the database for every test using the
        created template.
    
        - Change DropwizarAppWithPostgres to Environment rule only providing
        configuration and port where it runs.

--- src/test/java/uk/gov/pay/directdebit/infra/PostgresDockerRule.java
+++ src/test/java/uk/gov/pay/directdebit/infra/PostgresDockerRule.java
@@ -1,78 +1,82 @@
 package uk.gov.pay.directdebit.infra;
 
 import com.spotify.docker.client.DefaultDockerClient;
-import com.spotify.docker.client.DockerClient;
-import com.spotify.docker.client.exceptions.DockerCertificateException;
-import com.spotify.docker.client.exceptions.DockerException;
 import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
 
-import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Optional;
 
+import static java.sql.DriverManager.getConnection;
 import static org.junit.Assert.assertNotNull;
+import static uk.gov.pay.directdebit.infra.PostgresContainer.DB_PASSWORD;
+import static uk.gov.pay.directdebit.infra.PostgresContainer.DB_USERNAME;
 
 public class PostgresDockerRule implements TestRule {
 
-    private static String host;
-    private static PostgresContainer container;
-
+    static final String DB_NAME = "directdebitconnectortests";
     private static final String DOCKER_HOST = "DOCKER_HOST";
     private static final String DOCKER_CERT_PATH = "DOCKER_CERT_PATH";
+    private static final String HOST = dockerHostLocalAware();
+    private static PostgresContainer container;
 
-    static {
+    public PostgresDockerRule() {
         try {
-            String dockerHost = Optional.ofNullable(System.getenv(DOCKER_HOST)).
-                    orElseThrow(() -> new RuntimeException(DOCKER_HOST + " environment variable not set. It has to be set to the docker daemon location."));
-            URI dockerHostURI = new URI(dockerHost);
-            boolean isDockerDaemonLocal = "unix".equals(dockerHostURI.getScheme());
-            if(!isDockerDaemonLocal) {
+            startPostgresIfNecessary();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static URI getDockerHostUri() throws URISyntaxException {
+        final String dockerHost = Optional.ofNullable(System.getenv(DOCKER_HOST)).
+                orElseThrow(() -> new RuntimeException(DOCKER_HOST + " environment variable not set. It has to be set to the docker daemon location."));
+        return new URI(dockerHost);
+    }
+
+    private static String dockerHostLocalAware() {
+        try {
+            URI dockerHostURI = getDockerHostUri();
+            final boolean isDockerDaemonLocal = "unix".equals(dockerHostURI.getScheme());
+            if (isDockerDaemonLocal) {
+                return "localhost";
+            } else {
                 assertNotNull(DOCKER_CERT_PATH + " environment variable not set.", System.getenv(DOCKER_CERT_PATH));
+                return dockerHostURI.getHost();
             }
-            host = isDockerDaemonLocal ? "localhost" : dockerHostURI.getHost();
         } catch (URISyntaxException e) {
             throw new RuntimeException(e);
         }
     }
 
-    PostgresDockerRule() {
-        startPostgresIfNecessary();
-    }
-
-    String getConnectionUrl() {
-        return container.getConnectionUrl();
-    }
-
     @Override
     public Statement apply(Statement statement, Description description) {
+        createDatabase();
         return statement;
     }
 
-    private void startPostgresIfNecessary() {
-        try {
-            if (container == null) {
-                DockerClient docker = DefaultDockerClient.fromEnv().build();
-                container = new PostgresContainer(docker, host);
-            }
-        } catch (DockerCertificateException | InterruptedException | DockerException | IOException | ClassNotFoundException e) {
+    private void createDatabase() {
+        try (Connection connection = getConnection(container.getPostgresDbUri(), DB_USERNAME, DB_PASSWORD)) {
+            connection.createStatement().execute("CREATE DATABASE " + DB_NAME + " WITH owner=" + DB_USERNAME + " TEMPLATE postgres");
+            connection.createStatement().execute("GRANT ALL PRIVILEGES ON DATABASE " + DB_NAME + " TO " + DB_USERNAME);
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
-    String getUsername() {
-        return container.getUsername();
+    String getDbRootUri() {
+        return container.getPostgresDbUri();
     }
 
-    String getPassword() {
-        return container.getPassword();
-    }
 
-    void stop() {
-        container.stop();
-        container = null;
+    private void startPostgresIfNecessary() throws Exception {
+        if (container == null) {
+            container = new PostgresContainer(DefaultDockerClient.fromEnv().build(), HOST);
+        }
     }
 
     public String getDriverClass() {
