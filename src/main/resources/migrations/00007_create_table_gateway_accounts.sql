8fa92aa354f4cee2f19b46cfeb86771888d1d58e
Silvia Mandalà <sil.mandala@gmail.com>
2018-01-31 16:24:11 +0000

    PP-3061 Add gateway accounts table to the db

--- src/main/resources/migrations/00007_create_table_gateway_accounts.sql
+++ src/main/resources/migrations/00007_create_table_gateway_accounts.sql
@@ -3,11 +3,16 @@
 --changeset uk.gov.pay:add_table-gateway_accounts
 CREATE TABLE gateway_accounts (
     id BIGSERIAL PRIMARY KEY,
+    external_id VARCHAR(26),
     payment_provider VARCHAR(255) NOT NULL,
     service_name VARCHAR(50) NOT NULL,
     type VARCHAR(10) NOT NULL,
-    description VARCHAR(255) NOT NULL,
-    analytics_id VARCHAR(255) NOT NULL,
+    description VARCHAR(255),
+    analytics_id VARCHAR(255),
     version INTEGER DEFAULT 0 NOT NULL
 );
 --rollback drop table gateway_accounts;
+
+--changeset uk.gov.pay:add_gateway_accounts_external_id_idx
+CREATE UNIQUE INDEX gateway_accounts_external_id_idx ON gateway_accounts(external_id)
+--rollback drop index gateway_accounts_external_id_idx;
