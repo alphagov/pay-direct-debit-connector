4ca276363936a0155e788f6618b9cebebe8982ad
Danail Minchev <danail.minchev@gmail.com>
2018-02-05 17:05:23 +0000

    PP-3237 TransactionDao, TransactionService and PaymentState modifications
    - Added new findAllByPaymentState method which can be used to retrieve list of transactions by it's paymentState
    
    with @rauligs

--- src/main/java/uk/gov/pay/directdebit/payments/services/TransactionService.java
+++ src/main/java/uk/gov/pay/directdebit/payments/services/TransactionService.java
@@ -11,6 +11,7 @@ import uk.gov.pay.directdebit.payments.model.PaymentState;
 import uk.gov.pay.directdebit.payments.model.SandboxPaymentStatesGraph;
 import uk.gov.pay.directdebit.payments.model.Transaction;
 
+import java.util.List;
 import java.util.Optional;
 
 import static uk.gov.pay.directdebit.payments.model.PaymentRequestEvent.chargeCreated;
@@ -49,6 +50,10 @@ public class TransactionService {
         transaction.setId(id);
         paymentRequestEventService.insertEventFor(paymentRequest, chargeCreated(paymentRequest.getId()));
         return transaction;
+    }
+
+    public List<Transaction> findAllByPaymentState(PaymentState paymentState) {
+        return transactionDao.findAllByPaymentState(paymentState);
     }
 
     public Optional<Transaction> findChargeForToken(String token) {
