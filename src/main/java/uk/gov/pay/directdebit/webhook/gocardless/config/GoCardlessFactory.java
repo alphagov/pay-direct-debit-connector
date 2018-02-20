8bd66eab3577f66f75fda704adc3a8e613d89e3e
Silvia Mandalà <sil.mandala@gmail.com>
2018-02-20 15:53:49 +0000

    PP-2886 Inject baseUrl in gocardless client
    If `GDS_DIRECTDEBIT_CONNECTOR_GOCARDLESS_URL` environment variable is specified, then we instantiate a client which uses that as a baseUrl for gocardless calls. If it's not specified, the client is going to pick up the right GoCardless url based on the type of account (LIVE / TEST)

--- src/main/java/uk/gov/pay/directdebit/webhook/gocardless/config/GoCardlessFactory.java
+++ src/main/java/uk/gov/pay/directdebit/webhook/gocardless/config/GoCardlessFactory.java
@@ -2,12 +2,13 @@ package uk.gov.pay.directdebit.webhook.gocardless.config;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.gocardless.GoCardlessClient;
+import io.dropwizard.Configuration;
 import org.apache.commons.lang3.StringUtils;
 import uk.gov.pay.directdebit.webhook.gocardless.support.WebhookVerifier;
 
 import javax.validation.constraints.NotNull;
 
-public class GoCardlessFactory {
+public class GoCardlessFactory extends Configuration {
 
     @JsonProperty
     private String accessToken;
@@ -15,11 +16,18 @@ public class GoCardlessFactory {
     @JsonProperty
     private String webhookSecret;
 
+    // if specified, we are going to create a gocardless client which connects to that url. This can be used to send requests to our stubs instead of gocardless. Otherwise, we will hit the real gocardless and the url will depend on the environment (SANDBOX/LIVE)
+    @JsonProperty
+    private String clientUrl;
+
     @JsonProperty
     @NotNull
     private GoCardlessClient.Environment environment;
 
     public GoCardlessClient buildClient() {
+        if (clientUrl != null) {
+            return GoCardlessClient.create(accessToken, clientUrl);
+        }
         return GoCardlessClient.create(accessToken, environment);
     }
 
