2fc61bd53e74ea1a24b73ab5aa057ac5b5da7063
Silvia Mandalà <sil.mandala@gmail.com>
2018-02-26 10:11:29 +0000

    PP-2890 Add resources for gocardless stubs (mandate, payment)

--- src/test/java/uk/gov/pay/directdebit/util/TestRequestResponsesLoader.java
+++ src/test/java/uk/gov/pay/directdebit/util/TestRequestResponsesLoader.java
@@ -13,12 +13,16 @@ public class TestRequestResponsesLoader {
     private static final String REQUESTS_GOCARLDESS_BASE_NAME = REQUESTS_BASE_NAME + "/gocardless";
 
     //requests
-    public static final String GOCARDLESS_CREATE_CUSTOMER_REQUEST= REQUESTS_GOCARLDESS_BASE_NAME + "/create-customer.json";
+    public static final String GOCARDLESS_CREATE_CUSTOMER_REQUEST = REQUESTS_GOCARLDESS_BASE_NAME + "/create-customer.json";
     public static final String GOCARDLESS_CREATE_CUSTOMER_BANK_ACCOUNT_REQUEST = REQUESTS_GOCARLDESS_BASE_NAME + "/create-customer-bank-account.json";
+    public static final String GOCARDLESS_CREATE_MANDATE_REQUEST = REQUESTS_GOCARLDESS_BASE_NAME + "/create-mandate.json";
+    public static final String GOCARDLESS_CREATE_PAYMENT_REQUEST = REQUESTS_GOCARLDESS_BASE_NAME + "/create-payment.json";
 
     //responses
     public static final String GOCARDLESS_CREATE_CUSTOMER_SUCCESS_RESPONSE = RESPONSES_GOCARLDESS_BASE_NAME + "/create-customer-success.json";
     public static final String GOCARDLESS_CREATE_CUSTOMER_BANK_ACCOUNT_SUCCESS_RESPONSE = RESPONSES_GOCARLDESS_BASE_NAME + "/create-customer-bank-account-success.json";
+    public static final String GOCARDLESS_CREATE_MANDATE_SUCCESS_RESPONSE = RESPONSES_GOCARLDESS_BASE_NAME + "/create-mandate-success.json";
+    public static final String GOCARDLESS_CREATE_PAYMENT_SUCCESS_RESPONSE = RESPONSES_GOCARLDESS_BASE_NAME + "/create-payment-success.json";
 
     static public String load(String location) {
         try {
