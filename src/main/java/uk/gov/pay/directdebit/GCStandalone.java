146e1959d83b1ec8bbd68273bae46525a1bced4d
Belinda Cowey <belinda.cowey@digital.cabinet-office.gov.uk>
2018-03-02 10:11:59 +0000

    Added HTTP implementation to test a post without using the client

--- src/main/java/uk/gov/pay/directdebit/GCStandalone.java
+++ src/main/java/uk/gov/pay/directdebit/GCStandalone.java
@@ -6,9 +6,46 @@ import com.gocardless.resources.RedirectFlow;
 
 import java.util.List;
 import java.util.Arrays;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+//import org.json.simple.JSONObject;
 
 public class GCStandalone {
-    public static void main(String[] args) {
+/*
+    private static void httpsRequest() {
+        HttpClient httpClient = HttpClientBuilder.create().build(); //Use this instead
+
+        JSONObject customer = new JSONObject();
+        customer.put("email", "user@example.com");
+        customer.put("given_name", "Frank");
+        customer.put("family_name", "Osborne");
+
+        JSONObject customers = new JSONObject();
+        customers.put("customers", customer);
+
+	    try {
+            HttpPost post = new HttpPost("https://api-sandbox.gocardless.com/customers");
+	        StringEntity params =new StringEntity(customers.toString());
+            post.addHeader("content-type", "application/json");
+            post.addHeader("accept", "application/json");
+            post.addHeader("GoCardless-Version", "2015-07-06");
+            post.addHeader("authorization", System.getenv("GDS_DIRECTDEBIT_CONNECTOR_GOCARDLESS_ACCESS_TOKEN"));
+            post.setEntity(params);
+
+	        HttpResponse response = httpClient.execute(post);
+
+            System.out.println("Successfully posted json");
+            System.out.println(response.toString());
+	    } catch (Exception ex) {
+	        System.out.println("Failed to post request");
+	        System.out.println(ex.getMessage());
+	    }
+    }
+*/
+    private static void gcRequest() {
         System.out.println("Creating gocardless sandbox client");
 
         try {
@@ -20,42 +57,46 @@ public class GCStandalone {
                     // Change me to LIVE when you're ready to go live
                     GoCardlessClient.Environment.SANDBOX
             );
-            System.out.println("List of clients, empty array is good, it shows that we are authenticated");
 
-            try {
-                List<Customer> customers = client.customers().list().execute().getItems();
-                System.out.println(Arrays.toString(customers.toArray()));
-            } catch (Exception e) {
-                System.out.println("Unable to list customers");
-                System.out.println(e.getMessage());
-            }
+            System.out.println("List of clients:");
+            List<Customer> customers = client.customers().list().execute().getItems();
+            customers.forEach (customer-> {
+                System.out.println((com.gocardless.resources.Customer)customer);
+            });
 
-            System.out.println("Before redirect flow implementation");
+            System.out.println(Arrays.toString(customers.toArray()));
 
-            try {
-                RedirectFlow redirectFlow = client.redirectFlows().create()
-                        .withDescription("Cider Barrels") // This will be shown on the payment pages.
-                        .withSessionToken("dummy_session_token") // Not the access token
-                        .withSuccessRedirectUrl("https://developer.gocardless.com/example-redirect-uri/")
-                        // Optionally, prefill customer details on the payment page
-                        .withPrefilledCustomerGivenName("Tim")
-                        .withPrefilledCustomerFamilyName("Rogers")
-                        .withPrefilledCustomerEmail("tim@gocardless.com")
-                        .withPrefilledCustomerAddressLine1("338-346 Goswell Road")
-                        .withPrefilledCustomerCity("London")
-                        .withPrefilledCustomerPostalCode("EC1V 7LQ")
-                        .execute();
+            return;
+            /*System.out.println("Before redirect flow implementation");
 
-                // Hold on to this ID - you'll need it when you
-                // "confirm" the redirect flow later
-                System.out.println(redirectFlow.getId());
-                System.out.println(redirectFlow.getRedirectUrl());
-            } catch (Exception e) {
-                System.out.println("Unable to create redirect flow");
-                System.out.println(e.getMessage());
-            }
+            RedirectFlow redirectFlow = client.redirectFlows().create()
+                    .withDescription("Cider Barrels") // This will be shown on the payment pages.
+                    .withSessionToken("dummy_session_token") // Not the access token
+                    .withSuccessRedirectUrl("https://developer.gocardless.com/example-redirect-uri/")
+                    // Optionally, prefill customer details on the payment page
+                    .withPrefilledCustomerGivenName("Tim")
+                    .withPrefilledCustomerFamilyName("Rogers")
+                    .withPrefilledCustomerEmail("tim@gocardless.com")
+                    .withPrefilledCustomerAddressLine1("338-346 Goswell Road")
+                    .withPrefilledCustomerCity("London")
+                    .withPrefilledCustomerPostalCode("EC1V 7LQ")
+                    .execute();
+
+            // Hold on to this ID - you'll need it when you
+            // "confirm" the redirect flow later
+            System.out.println(redirectFlow.getId());
+            System.out.println(redirectFlow.getRedirectUrl());*/
         } catch (Exception e) {
-            System.out.println("Unable to create GC client");
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            //httpsRequest();
+            gcRequest();
+        } catch (Exception e) {
+            System.out.println("Failed ....");
             System.out.println(e.getMessage());
         }
     }
