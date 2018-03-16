1b65e402827c4a8b305542c35ba61fb96d9da609
Belinda Cowey <belinda.cowey@digital.cabinet-office.gov.uk>
2018-03-16 10:47:29 +0000

    PP-3443 Run a payment flow with GoCardless Sandbox
    Remove the cacerts testing as we are going to put the cacert required
    into the cacert file when the instance is built, see
    https://github.com/alphagov/pay-infra/pull/1396/files
    
    solo @belindac

--- docker-startup.sh
+++ docker-startup.sh
@@ -4,14 +4,14 @@ set -eu
 RUN_MIGRATION=${RUN_MIGRATION:-false}
 RUN_APP=${RUN_APP:-true}
 
-java -Djavax.net.ssl.trustStore=/app/cacerts -jar *-allinone.jar waitOnDependencies *.yaml
+java -jar *-allinone.jar waitOnDependencies *.yaml
 
 if [ "$RUN_MIGRATION" == "true" ]; then
-  java -Djavax.net.ssl.trustStore=/app/cacerts -jar *-allinone.jar db migrate *.yaml
+  java -jar *-allinone.jar db migrate *.yaml
 fi
 
 if [ "$RUN_APP" == "true" ]; then
-  java -Djavax.net.ssl.trustStore=/app/cacerts -jar *-allinone.jar server *.yaml
+  java -jar *-allinone.jar server *.yaml
 fi
 
 exit 0
