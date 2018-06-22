ef0741111df2abdf4d1c5cd174e473ae17a754fa
Dan Worth <danworth@gds5062.home>
2018-06-22 08:32:01 +0100

    Added logging to expireService.

--- src/main/java/uk/gov/pay/directdebit/tasks/services/ExpireService.java
+++ src/main/java/uk/gov/pay/directdebit/tasks/services/ExpireService.java
@@ -2,6 +2,8 @@ package uk.gov.pay.directdebit.tasks.services;
 
 import com.gocardless.resources.Payment;
 import com.google.errorprone.annotations.DoNotMock;
+import org.slf4j.Logger;
+import uk.gov.pay.directdebit.app.logger.PayLoggerFactory;
 import uk.gov.pay.directdebit.mandate.model.Mandate;
 import uk.gov.pay.directdebit.mandate.model.MandateState;
 import uk.gov.pay.directdebit.mandate.model.MandateStatesGraph;
@@ -21,7 +23,9 @@ public class ExpireService {
     private TransactionService transactionService;
     private MandateStatesGraph mandateStatesGraph;
     private PaymentStatesGraph paymentStatesGraph;
-    
+    private static final Logger LOGGER = PayLoggerFactory.getLogger(ExpireService.class);
+    private final long MIN_EXPIRY_AGE_MINUTES = 90l;
+    private final PaymentState PAYMENT_EXPIRY_CUTOFF_STATUS = PaymentState.PENDING;
     
     @Inject
     public ExpireService(TransactionService transactionService, MandateStatesGraph mandateStatesGraph, PaymentStatesGraph paymentStatesGraph) {
@@ -31,16 +35,20 @@ public class ExpireService {
     }
     
     public int expirePayments() {
+        LOGGER.info("Starting expire payments process.");
         List<Transaction> paymentsToExpire = getPaymentsForExpiration();
+        int expirePaymentsCounter = 0;
         for (Transaction payment : paymentsToExpire) {
             transactionService.paymentExpired(payment);
+            LOGGER.info("Expired payment " + payment.getId());
+            expirePaymentsCounter++;
         }
-        return paymentsToExpire.size();
+        return expirePaymentsCounter;
     }
 
     public List<Transaction> getPaymentsForExpiration() {
-        Set<PaymentState> states = paymentStatesGraph.getPriorStates(PaymentState.PENDING);
-        ZonedDateTime cutOffTime = ZonedDateTime.now().minusMinutes(90l);
+        Set<PaymentState> states = paymentStatesGraph.getPriorStates(PAYMENT_EXPIRY_CUTOFF_STATUS);
+        ZonedDateTime cutOffTime = ZonedDateTime.now().minusMinutes(MIN_EXPIRY_AGE_MINUTES);
         return transactionService.findAllPaymentsBySetOfStatesAndCreationTime(states, cutOffTime);
     }
     
