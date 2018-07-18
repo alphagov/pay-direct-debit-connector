28bec439abd717df56b186130c57dc3f0146b380
Danail Minchev <danail.minchev@gmail.com>
2018-07-18 17:49:37 +0100

    PP-3966 Modified `PaymentProviderAccessToken` and `PaymentProviderOrganisationIdentifier` to have `value` field
    - Modified `PaymentProviderAccessToken` and `PaymentProviderOrganisationIdentifier` to have `value` field for consistency which is used in Jdbi queries
    
    with @SandorArpa

--- src/main/java/uk/gov/pay/directdebit/gatewayaccounts/model/PaymentProviderAccessToken.java
+++ src/main/java/uk/gov/pay/directdebit/gatewayaccounts/model/PaymentProviderAccessToken.java
@@ -4,33 +4,37 @@ import java.util.Objects;
 
 public class PaymentProviderAccessToken {
 
-    private final String paymentProviderAccessToken;
+    private final String value;
 
-    private PaymentProviderAccessToken(String paymentProviderAccessToken) {
-        this.paymentProviderAccessToken = Objects.requireNonNull(paymentProviderAccessToken);
+    private PaymentProviderAccessToken(String value) {
+        this.value = Objects.requireNonNull(value);
     }
 
-    public static PaymentProviderAccessToken of(String paymentProviderAccessToken) {
-        return new PaymentProviderAccessToken(paymentProviderAccessToken);
+    public static PaymentProviderAccessToken of(String value) {
+        return new PaymentProviderAccessToken(value);
+    }
+
+    public String getValue() {
+        return value;
     }
 
     @Override
     public boolean equals(Object other) {
         if (other != null && other.getClass() == PaymentProviderAccessToken.class) {
             PaymentProviderAccessToken that = (PaymentProviderAccessToken) other;
-            return this.paymentProviderAccessToken.equals(that.paymentProviderAccessToken);
+            return this.value.equals(that.value);
         }
         return false;
     }
 
     @Override
     public int hashCode() {
-        return paymentProviderAccessToken.hashCode();
+        return value.hashCode();
     }
 
     @Override
     public String toString() {
-        return paymentProviderAccessToken;
+        return value;
     }
-    
+
 }
--- src/main/java/uk/gov/pay/directdebit/gatewayaccounts/model/PaymentProviderOrganisationIdentifier.java
+++ src/main/java/uk/gov/pay/directdebit/gatewayaccounts/model/PaymentProviderOrganisationIdentifier.java
@@ -4,33 +4,37 @@ import java.util.Objects;
 
 public class PaymentProviderOrganisationIdentifier {
 
-    private final String paymentProviderOrganisationIdentifier;
+    private final String value;
 
-    private PaymentProviderOrganisationIdentifier(String paymentProviderOrganisationIdentifier) {
-        this.paymentProviderOrganisationIdentifier = Objects.requireNonNull(paymentProviderOrganisationIdentifier);
+    private PaymentProviderOrganisationIdentifier(String value) {
+        this.value = Objects.requireNonNull(value);
     }
 
-    public static PaymentProviderOrganisationIdentifier of(String paymentProviderAccessToken) {
-        return new PaymentProviderOrganisationIdentifier(paymentProviderAccessToken);
+    public static PaymentProviderOrganisationIdentifier of(String value) {
+        return new PaymentProviderOrganisationIdentifier(value);
+    }
+
+    public String getValue() {
+        return value;
     }
 
     @Override
     public boolean equals(Object other) {
         if (other != null && other.getClass() == PaymentProviderOrganisationIdentifier.class) {
             PaymentProviderOrganisationIdentifier that = (PaymentProviderOrganisationIdentifier) other;
-            return this.paymentProviderOrganisationIdentifier.equals(that.paymentProviderOrganisationIdentifier);
+            return this.value.equals(that.value);
         }
         return false;
     }
 
     @Override
     public int hashCode() {
-        return paymentProviderOrganisationIdentifier.hashCode();
+        return value.hashCode();
     }
 
     @Override
     public String toString() {
-        return paymentProviderOrganisationIdentifier;
+        return value;
     }
-    
+
 }
