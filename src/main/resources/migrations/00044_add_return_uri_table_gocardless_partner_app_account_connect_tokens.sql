1d4184e4b2cdba55a9411df3c02c059171a3a367
george racu <george.racu@digital.cabinet-office.gov.uk>
2018-08-07 12:42:39 +0100

    PP-4043 Renamed column return_uri to redirect_uri
    - To be inline with the name of the parameter from GoCardless request
    - Actually is a redirect uri, not return as it will redirect the user
    and not return the response

--- src/main/resources/migrations/00044_add_return_uri_table_gocardless_partner_app_account_connect_tokens.sql
+++ src/main/resources/migrations/00044_add_return_uri_table_gocardless_partner_app_account_connect_tokens.sql
@@ -1,5 +1,5 @@
 --liquibase formatted sql
 
---changeset uk.gov.pay:add_column-gocardless_partner_app_account_connect_tokens-add-return_uri
-ALTER TABLE gocardless_partner_app_account_connect_tokens ADD COLUMN return_uri VARCHAR(255);
---rollback ALTER TABLE gocardless_partner_app_account_connect_tokens DROP COLUMN return_uri;
+--changeset uk.gov.pay:add_column-gocardless_partner_app_account_connect_tokens-add-redirect_uri
+ALTER TABLE gocardless_partner_app_account_connect_tokens ADD COLUMN redirect_uri VARCHAR(255);
+--rollback ALTER TABLE gocardless_partner_app_account_connect_tokens DROP COLUMN redirect_uri;
