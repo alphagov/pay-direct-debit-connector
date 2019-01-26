df46e08017b7cda9562853da566abc33d47edcb4
Russell Howe <russell.howe@digital.cabinet-office.gov.uk>
2019-01-26 10:16:17 +0000

    Simplify GoCardlessAppConnectAccountStateResourceTest
    This test had lots of unused things in it. Strip it back to basics.

--- src/test/java/uk/gov/pay/directdebit/partnerapp/resources/GoCardlessAppConnectAccountStateResourceTest.java
+++ src/test/java/uk/gov/pay/directdebit/partnerapp/resources/GoCardlessAppConnectAccountStateResourceTest.java
@@ -1,6 +1,5 @@
 package uk.gov.pay.directdebit.partnerapp.resources;
 
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -11,10 +10,6 @@ import uk.gov.pay.directdebit.common.exception.BadRequestException;
 import uk.gov.pay.directdebit.partnerapp.services.GoCardlessAppConnectAccountService;
 
 import java.util.HashMap;
-import java.util.Map;
-
-import static uk.gov.pay.directdebit.partnerapp.resources.GoCardlessAppConnectAccountStateResource.GATEWAY_ACCOUNT_ID_FIELD;
-import static uk.gov.pay.directdebit.partnerapp.resources.GoCardlessAppConnectAccountStateResource.REDIRECT_URI_FIELD;
 
 @RunWith(MockitoJUnitRunner.class)
 public class GoCardlessAppConnectAccountStateResourceTest {
@@ -25,20 +20,9 @@ public class GoCardlessAppConnectAccountStateResourceTest {
     @Mock
     private GoCardlessAppConnectAccountService mockedTokenService;
 
-    private GoCardlessAppConnectAccountStateResource resource;
-    private static final String GATEWAY_EXTERNAL_ID = "some-test-test-external-id";
-    private static final String REDIRECT_URI = "https://example.com/oauth/complete";
-    private final Map<String, String> REQUEST_MAP = new HashMap<>();
-
-    @Before
-    public void setUp() {
-        REQUEST_MAP.put(GATEWAY_ACCOUNT_ID_FIELD, GATEWAY_EXTERNAL_ID);
-        REQUEST_MAP.put(REDIRECT_URI_FIELD, REDIRECT_URI);
-        resource = new GoCardlessAppConnectAccountStateResource(mockedTokenService);
-    }
-
     @Test
     public void createToken_shouldReturnBadRequest_whenMissingGatewayAccountFromRequest() {
+        GoCardlessAppConnectAccountStateResource resource = new GoCardlessAppConnectAccountStateResource(mockedTokenService);
         thrown.expect(BadRequestException.class);
         thrown.expectMessage("Field(s) missing: [gateway_account_id, redirect_uri]");
         thrown.reportMissingExceptionWithMessage("BadRequestException expected");
