6232faa5a4c13ae70bccc5cc6a0743e3b3253653
Russell Howe <russell.howe@digital.cabinet-office.gov.uk>
2019-01-30 08:17:34 +0000

    Remove unnecessary initialisers

--- src/main/java/uk/gov/pay/directdebit/payments/api/CollectPaymentResponse.java
+++ src/main/java/uk/gov/pay/directdebit/payments/api/CollectPaymentResponse.java
@@ -9,7 +9,6 @@ import uk.gov.pay.directdebit.payments.model.Transaction;
 
 import java.net.URI;
 import java.time.ZonedDateTime;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -19,7 +18,7 @@ import static com.fasterxml.jackson.annotation.JsonInclude.Include;
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public class CollectPaymentResponse {
     @JsonProperty("links")
-    private List<Map<String, Object>> dataLinks = new ArrayList<>();
+    private List<Map<String, Object>> dataLinks;
 
     @JsonProperty("charge_id")
     private String transactionExternalId;
