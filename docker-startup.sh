7c3d98363ef2581bb91601408d4f3baca2530923
Russell Howe <russell.howe@digital.cabinet-office.gov.uk>
2019-01-31 07:44:10 +0000

    PP-4864: Use the default Java truststore for TLS
    Prior to this, we ignored the default Java truststore and built our own using
    the certificates from the directory specified by the CERTS_PATH environment
    variable.
    
    We can instead rely on the default Java truststore, although there are cases
    where we need to add additional CA certificates:
    
    * Amazon RDS has its own CA certificate. We add this to the default Java
      truststore when building our base container image.
    * During our end to end tests, we supply custom certificates in order to run
      TLS in our docker-compose environment.
    
    Handle these custom certificates in the existing way, using CERTS_PATH to point
    to them and have the shell wrapper add them to the default Java truststore
    prior to starting the application.
    
    By default, the PostgreSQL JDBC driver does not use the default Java truststore
    to validate server certificates. Instead, it defaults to looking for
    ${user.home}/.postgresql/root.crt
    
    Rather than supply this file specifically, we can use the provided
    org.postgresql.ssl.DefaultJavaSSLFactory which will consult the default Java
    truststore instead.

--- docker-startup.sh
+++ docker-startup.sh
@@ -4,6 +4,17 @@ set -eu
 
 RUN_MIGRATION=${RUN_MIGRATION:-false}
 RUN_APP=${RUN_APP:-true}
+JAVA_OPTS=${JAVA_OPTS:-}
+
+if [ -n "${CERTS_PATH:-}" ]; then
+  i=0
+  truststore=/etc/ssl/certs/java/cacerts
+  truststore_pass=changeit
+  for cert in "$CERTS_PATH"/*; do
+    echo "Adding $cert to $truststore"
+    [ ! -f "$cert" ] || keytool -importcert -noprompt -keystore "$truststore" -storepass "$truststore_pass" -file "$cert" -alias custom$((i++))
+  done
+fi
 
 java $JAVA_OPTS -jar *-allinone.jar waitOnDependencies *.yaml
 
