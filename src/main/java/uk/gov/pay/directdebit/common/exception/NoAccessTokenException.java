e8238be1ddb7b5733fec620f58117146f806006a
Rory Malcolm <rory.malcolm@digital.cabinet-office.gov.uk>
2019-06-11 09:51:11 +0100

    Revert "Merge pull request #486 from alphagov/PP-5278-block-unlinked-gocardless-mandate-creation"
    This reverts commit 7c9491371f030c66343102485a2c954abf473214, reversing
    changes made to 480c3a82067f1213991a93bb10813231c1ac40ef.

--- src/main/java/uk/gov/pay/directdebit/common/exception/NoAccessTokenException.java
+++
@@ -1,7 +0,0 @@
-package uk.gov.pay.directdebit.common.exception;
-
-public class NoAccessTokenException extends RuntimeException {
-    public NoAccessTokenException(String message){
-        super(message);
-    }
-}
--- src/main/java/uk/gov/pay/directdebit/common/exception/UnlinkedGCMerchantAccountException.java
+++
@@ -1,7 +0,0 @@
-package uk.gov.pay.directdebit.common.exception;
-
-public class UnlinkedGCMerchantAccountException extends RuntimeException {
-    public UnlinkedGCMerchantAccountException(String message){
-        super(message);
-    }
-}
