9a8e20aacc417bd496f575d6388dd9f417532763
Rory Malcolm <rory.malcolm@digital.cabinet-office.gov.uk>
2019-06-26 15:14:45 +0100

    PP-5300 Refactor DD connector to remove unused fields on GET Payment
    - The response from getCharge() in PaymentsResource now maps
      onto what is expected in publicAPI
    - Changes the return type from PaymentResponse to
      CollectPaymentResponse

--- src/main/java/uk/gov/pay/directdebit/payments/resources/PaymentResource.java
+++ src/main/java/uk/gov/pay/directdebit/payments/resources/PaymentResource.java
@@ -7,7 +7,6 @@ import uk.gov.pay.directdebit.gatewayaccounts.model.GatewayAccount;
 import uk.gov.pay.directdebit.payments.api.CollectPaymentRequest;
 import uk.gov.pay.directdebit.payments.api.CollectPaymentRequestValidator;
 import uk.gov.pay.directdebit.payments.api.CollectPaymentResponse;
-import uk.gov.pay.directdebit.payments.api.PaymentResponse;
 import uk.gov.pay.directdebit.payments.model.Payment;
 import uk.gov.pay.directdebit.payments.services.CollectService;
 import uk.gov.pay.directdebit.payments.services.PaymentService;
@@ -24,7 +23,7 @@ import javax.ws.rs.core.UriInfo;
 import java.util.Map;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
-import static javax.ws.rs.core.Response.created;
+import static org.apache.http.HttpStatus.SC_CREATED;
 
 @Path("/")
 public class PaymentResource {
@@ -48,7 +47,7 @@ public class PaymentResource {
     @Produces(APPLICATION_JSON)
     @Timed
     public Response getCharge(@PathParam("accountId") String accountExternalId, @PathParam("paymentExternalId") String transactionExternalId, @Context UriInfo uriInfo) {
-        PaymentResponse response = paymentService.getPaymentWithExternalId(accountExternalId, transactionExternalId, uriInfo);
+        CollectPaymentResponse response = paymentService.getPaymentWithExternalId(transactionExternalId);
         return Response.ok(response).build();
     }
 
@@ -56,11 +55,11 @@ public class PaymentResource {
     @Path("/v1/api/accounts/{accountId}/charges/collect")
     @Produces(APPLICATION_JSON)
     @Timed
-    public Response collectPaymentFromMandate(@PathParam("accountId") GatewayAccount gatewayAccount, Map<String, String> collectPaymentRequestMap, @Context UriInfo uriInfo) {
+    public Response collectPaymentFromMandate(@PathParam("accountId") GatewayAccount gatewayAccount, Map<String, String> collectPaymentRequestMap) {
         LOGGER.info("Received collect payment from mandate request");
         collectPaymentRequestValidator.validate(collectPaymentRequestMap);
         Payment paymentToCollect = collectService.collect(gatewayAccount, CollectPaymentRequest.of(collectPaymentRequestMap));
-        CollectPaymentResponse response = paymentService.collectPaymentResponseWithSelfLink(paymentToCollect, gatewayAccount.getExternalId(), uriInfo);
-        return created(response.getLink("self")).entity(response).build();
+        CollectPaymentResponse response = CollectPaymentResponse.from(paymentToCollect);
+        return Response.status(SC_CREATED).entity(response).build();
     }
 }
