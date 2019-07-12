2540125aadda7427a4a435301ac4c354602f08c4
danworth <dan.worth@digital.cabinet-office.gov.uk>
2019-07-12 09:41:09 +0100

    PP-5302 Fix date assertion in test
    I wrote the test MandateSearchParamsTest.shouldCreateQueryWithAllParams()
    which would only ever pass on the 11 July 2019. This fixes the assertion to pass
    on any day, please forgive me.

--- src/test/java/uk/gov/pay/directdebit/mandate/params/MandateSearchParamsTest.java
+++ src/test/java/uk/gov/pay/directdebit/mandate/params/MandateSearchParamsTest.java
@@ -13,20 +13,20 @@ import static uk.gov.pay.directdebit.mandate.params.MandateSearchParams.MandateS
 
 public class MandateSearchParamsTest {
 
-    private final String gatewayAccountExternalId = "expectedGatewayAccountExternalId";
-    private final String toDate = LocalDate.now().toString();
-    private final String fromDate = LocalDate.now().minusDays(3).toString();
-    private final String email = "tester@example.com";
-    private final int displaySize = 100;
-    private final String name = "tester";
-    private final String serviceReference = "aServiceReference";
-    private final MandateBankStatementReference mandateBankStatementReference = MandateBankStatementReference.valueOf("bankReference");
-    private final int page = 1;
-    private final MandateState mandateState = MandateState.PENDING;
-
-
     @Test
     public void shouldCreateQueryWithAllParams() {
+        int page = 1;
+        int displaySize = 100;
+        String email = "tester@example.com";
+        String name = "tester";
+        String serviceReference = "aServiceReference";
+        String toDate = LocalDate.now().toString();
+        String fromDate = LocalDate.now().minusDays(3).toString();
+        MandateState mandateState = MandateState.PENDING;
+        MandateBankStatementReference mandateBankStatementReference = 
+                MandateBankStatementReference.valueOf("bankReference");
+        
+        
         var mandateSearchParams = aMandateSearchParams()
                 .withFromDate(fromDate)
                 .withToDate(toDate)
@@ -39,15 +39,15 @@ public class MandateSearchParamsTest {
                 .withPage(page)
                 .build();
 
-        String expectedQuery = "page=1" +
-                "&display_size=100" +
-                "&reference=aServiceReference" +
-                "&state=PENDING" +
-                "&bank_statement_reference=bankReference" +
-                "&name=tester" +
-                "&email=tester@example.com" +
-                "&from_date=2019-07-08" +
-                "&to_date=2019-07-11";
+        String expectedQuery = "page=" + page +
+                "&display_size=" + displaySize +
+                "&reference=" + serviceReference +
+                "&state=" + mandateState +
+                "&bank_statement_reference=" + mandateBankStatementReference.toString() +
+                "&name=" + name +
+                "&email=" + email +
+                "&from_date=" + fromDate +
+                "&to_date=" + toDate;
 
         assertThat(mandateSearchParams.buildQueryParamString(), is(expectedQuery));
     }
