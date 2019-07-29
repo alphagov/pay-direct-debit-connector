0163d12e6dbccde14eadb3412ac7fa78152357a7
Oswald Quek <oswald.quek@digital.cabinet-office.gov.uk>
2019-07-29 13:58:51 +0100

    bau: Revert accidental tag deletion

--- src/test/java/uk/gov/pay/directdebit/pact/PublicApiContractTest.java
+++ src/test/java/uk/gov/pay/directdebit/pact/PublicApiContractTest.java
@@ -31,7 +31,7 @@ import static uk.gov.pay.directdebit.payments.fixtures.PaymentFixture.aPaymentFi
 
 @RunWith(PactRunner.class)
 @Provider("direct-debit-connector")
-@PactBroker(scheme = "https", host = "pact-broker-test.cloudapps.digital", tags = {"master", "test", "staging", "production"},
+@PactBroker(scheme = "https", host = "pact-broker-test.cloudapps.digital", tags = {"${PACT_CONSUMER_TAG}", "test", "staging", "production"},
         authentication = @PactBrokerAuth(username = "${PACT_BROKER_USERNAME}", password = "${PACT_BROKER_PASSWORD}"),
         consumers = {"publicapi"})
 //uncommenting the below is useful for testing pacts locally. grab the pact from the broker and put it in /pacts
