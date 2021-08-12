17ce89ef6114e13e342dba09bc92872ae1017e42
Sandor Arpa <sandor.arpa@digital.cabinet-office.gov.uk>
2021-08-12 16:45:10 +0100

    PP-8394  add standard charset to getBytes()
    - add StandardCharsets.UTF_8 to getBytes so it's not using locale

--- src/main/java/uk/gov/pay/directdebit/webhook/gocardless/support/GoCardlessWebhookVerifier.java
+++ src/main/java/uk/gov/pay/directdebit/webhook/gocardless/support/GoCardlessWebhookVerifier.java
@@ -3,6 +3,7 @@ package uk.gov.pay.directdebit.webhook.gocardless.support;
 import uk.gov.pay.directdebit.webhook.gocardless.exception.InvalidWebhookException;
 
 import javax.inject.Inject;
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 
 import static java.lang.String.format;
@@ -23,7 +24,7 @@ public class GoCardlessWebhookVerifier {
     private void verifySignature(String body, String expectedSignature) {
         String computedSignature = goCardlessWebhookSignatureCalculator.calculate(body);
 
-        if (!MessageDigest.isEqual(expectedSignature.getBytes(), computedSignature.getBytes())) {
+        if (!MessageDigest.isEqual(expectedSignature.getBytes(StandardCharsets.UTF_8), computedSignature.getBytes(StandardCharsets.UTF_8))) {
             throw new InvalidWebhookException(format("Invalid GoCardless webhook signature, received %s but computed %s",
                     expectedSignature, computedSignature));
         }
